b3e57406b676a739a5cb2938b5b6b2571dd4083d
Валентин <walik19988@gmail.com>
2017-10-20 16:56:41 +0300

    Final commit

--- src/java/homework/task1/Cat.java
+++ src/java/homework/task1/Cat.java
@@ -1,4 +1,84 @@
 package java.homework.task1;
 
+import java.awt.*;
+
 public class Cat {
+    private String name;
+    private int age;
+    private String breed;
+    private Color color;
+    private boolean sleep;
+
+    public Cat(String name, int age, String breed, Color color) {
+
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.color = color;
+        this.sleep = false;
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public boolean isSleep() {
+        return sleep;
+    }
+
+    public void setSleep(boolean sleep) {
+        this.sleep = sleep;
+    }
+
+    public void goToSleep() {
+        if(!this.sleep)
+            this.sleep = true;
+    }
+
+    public void wakeUp() {
+        if (this.sleep)
+            this.sleep = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                ", color=" + color +
+                ", sleep=" + sleep +
+                '}';
+    }
 }
--- src/java/homework/task1/Main.java
+++ src/java/homework/task1/Main.java
@@ -1,7 +1,15 @@
 package java.homework.task1;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Cat cat1 = new Cat("Mark", 4 , "british", Color.gray );
+        Cat cat2 = new Cat("Villem", 5, "british", Color.BLUE );
+        cat1.setAge(5);
+        cat2.setName("Hank");
+        cat1.goToSleep();
+        System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
